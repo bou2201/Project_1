6c9120f73832089e0a4876c53baa27ddf629adfc
bou2201 <lamchuc123456@gmail.com>
2021-09-17 22:49:37 +0700

    update

--- DALTest/SellerDALTest.cs
+++ DALTest/SellerDALTest.cs
@@ -10,62 +10,26 @@ namespace DALTest
         private SellerDAL dal = new SellerDAL();
         private Seller seller = new Seller();
 
-        // right TEST
         [Fact]
         public void LoginTest1()
         {
-            seller.Username = "admin2002";
+            seller.Username = "admin1";
             seller.Password = "Menswear22@";
             var result = dal.Login(seller);
             Assert.True(result);
         }
 
-        [Fact]
-        public void LoginTest2()
+        [Theory]
+        [InlineData("admin", "Menswear22@")]
+        [InlineData("admin2", "menswear@")]
+        [InlineData("", "Menswear22@")]
+        [InlineData("", "")]
+        public void LoginTest2(string Username, string Password)
         {
-            seller.Username = "admin";
-            seller.Password = "Menswear22@";
+            seller.Username = Username;
+            seller.Password = Password;
             var result = dal.Login(seller);
             Assert.False(result);
         }
-
-        [Fact]
-        public void LoginTest3()
-        {
-            seller.Username = "admin2002";
-            seller.Password = "menswear@";
-            var result = dal.Login(seller);
-            Assert.False(result);
-        }
-
-        [Fact]
-        public void LoginTest4()
-        {
-            seller.Username = "";
-            seller.Password = "Menswear22@";
-            var result = dal.Login(seller);
-            Assert.False(result);
-        }
-
-        [Fact]
-        public void LoginTest5()
-        {
-            seller.Username = "";
-            seller.Password = "";
-            var result = dal.Login(seller);
-            Assert.False(result);
-        }
-
-        // wrong TEST
-        // [Theory]
-        // [InlineData("admin20023", "PF13VTCAcademy")]
-        // [InlineData("admin20025", "PF13VTCAcademyis")]
-        // public void LoginTest2(string Username, string Password)
-        // {
-        //     seller.Username = Username;
-        //     seller.Password = Password;
-        //     var result = dal.Login(seller);
-        //     Assert.False(result);
-        // }
     }
 }
